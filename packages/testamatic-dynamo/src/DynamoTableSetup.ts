c004ab6e78b9fabd0192382e8c9a9e534f82c8c1
BlairJ <blair.jacobs@hypr.co.nz>
2024-12-20 10:22:18 +1300

    Add table name to dynamo setup and tear down message

--- packages/testamatic-dynamo/src/DynamoTableSetup.ts
+++ packages/testamatic-dynamo/src/DynamoTableSetup.ts
@@ -17,7 +17,7 @@ export class DynamoTableSetup implements Given {
     const exists = await this.tableExists()
     if (!exists) {
       await this.dynamoDB.send(new CreateTableCommand(this.tableDefinition))
-      console.log("dynamo table setup complete")
+      console.log(`dynamo table setup complete, ${this.tableDefinition.TableName}`)
     }
   }
 
@@ -25,7 +25,7 @@ export class DynamoTableSetup implements Given {
     const exists = await this.tableExists()
     if (exists) {
       await this.dynamoDB.send(new DeleteTableCommand({ TableName: this.tableDefinition.TableName }))
-      console.log("dynamo tesrdown complete")
+      console.log(`dynamo teardown complete, ${this.tableDefinition.TableName}`)
     }
   }
 
@@ -67,3 +67,51 @@ export const simpleTableDefinitionMaker = (
     WriteCapacityUnits: 1,
   },
 })
+
+async function waitForDynamoTableStatus(
+  tableName: string,
+  timeoutMs: number,
+  statusPredicate: (status: string | undefined) => boolean,
+  intervalMs: number
+): Promise<TableStatus | undefined> {
+  const pollTable = async (): Promise<TableStatus | undefined> => {
+    try {
+      const tableState = await client.send(new DescribeTableCommand({ TableName: tableName }))
+      return tableState.Table?.TableStatus
+    } catch (e) {
+      // Table has gone so dont raise an error...
+      if (e instanceof ResourceNotFoundException) return
+      throw e
+    }
+  }
+
+  return new Promise<TableStatus | undefined>((resolve, reject) => {
+    let timeout = timeoutMs
+    const interval = setInterval(async () => {
+      try {
+        const status = await pollTable()
+        timeout -= intervalMs
+        // console.log(`Status: ${status}, timeoutMs: ${timeoutMs}, Remaining: ${timeout}`)
+
+        if (statusPredicate(status)) {
+          clearInterval(interval)
+          return resolve(status)
+        }
+
+        logger.info(`Table Status: ${status} retrying`)
+
+        if (timeout <= 0) {
+          clearInterval(interval)
+          const errorMessage = `Timeout Wait For Dynamo Table(${tableName}) Status:${status}`
+          logger.error(errorMessage, null)
+          return reject(new Error(errorMessage))
+        }
+      } catch (e) {
+        clearInterval(interval)
+        const errorMessage = `Unexpected error Wait For Dynamo Table(${tableName})`
+        logger.error(errorMessage, null)
+        return reject(e)
+      }
+    }, intervalMs)
+  })
+}
